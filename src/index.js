8e32a64993a701c76a7147742d8b11378a318fc3
zakeyah <mom11omar@gmail.com>
2021-07-08 01:25:10 +0300

    socket...

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import ReactDOM from "react-dom";
 import App from "./app";
+import SettingsProvider from './contaxt/biddingContext'
 
 const MyApp = () => {
   return (
     <>
+    <SettingsProvider>
       <App />
+      </SettingsProvider>
     </>
   );
 };
